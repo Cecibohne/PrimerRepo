a73498056c1b40f930587f36db54c8dbb9cde6ae
Cecilia <ceecybohne@gmail.com>
2022-02-04 20:01:29 -0300

    agrego linea

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola chicas soy ceci");
         System.out.println("Hola Ceci soy Lina");
+        System.out.println("Hola chicas");
     }
 }
